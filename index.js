a0eb0c2c4a8796dae1aa0d0ae0e2860ab58502b6
Claus_Maslov <137815444+ClausMaslov@users.noreply.github.com>
2025-05-16 22:52:30 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 const RPC = require('discord-rpc');
+const http = require('http'); 
 const client = new RPC.Client({ transport: 'ipc' });
 
 const applicationId = '1373031533195038840'; 
 
+const server = http.createServer((req, res) => {
+    res.writeHead(200);
+    res.end('OK');
+});
+server.listen(process.env.PORT || 3000);
+
 client.on('ready', () => {
     client.setActivity({
-        // details: 'Играет в Minecraft 😊', 
-        // state: '', 
+        // details: 'Играет в Minecraft 😊', // Добавил текст для статуса
+        state: '', 
         startTimestamp: new Date(), 
         largeImageKey: 'plakat_bumazhnyy_minecraft_v_kino_pchela', 
         largeImageText: 'Сделал: Claus_Maslov :3',
-		type: 3,
+        type: 0, 
     });
     console.log('Статус установлен!');
 });
